a5e1b2f3821b0f8f34f15903ed02e60a918b987e
VaqueroFontenla <loret.vaquero@gmail.com>
2020-04-16 19:52:14 +0200

    Updates

---
+++ precache-manifest.c115f2ee75cedd2deb0281338b3500a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26716882dac7c4e7a889fa0cbfa88fdf",
+    "url": "/oxygen_tank_crono/index.html"
+  },
+  {
+    "revision": "c19ecd632b34cfe8f774",
+    "url": "/oxygen_tank_crono/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "f8bbb026495a147f72e2",
+    "url": "/oxygen_tank_crono/static/css/main.56ebcbe0.chunk.css"
+  },
+  {
+    "revision": "c19ecd632b34cfe8f774",
+    "url": "/oxygen_tank_crono/static/js/2.adff8443.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/oxygen_tank_crono/static/js/2.adff8443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bbb026495a147f72e2",
+    "url": "/oxygen_tank_crono/static/js/main.6e1d5a8f.chunk.js"
+  },
+  {
+    "revision": "d634677f0668372d8945",
+    "url": "/oxygen_tank_crono/static/js/runtime-main.39b06ef9.js"
+  }
+]);
